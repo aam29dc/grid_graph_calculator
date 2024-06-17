22d8afc7dbc0ac852f0c943d3499638f331cfc1e
Michael Arrotta <73267302+aam29dc@users.noreply.github.com>
2024-06-17 03:31:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ auto main() -> int {
 		SDL_RenderClear(Grid::getGrid()->renderer);
 
         //projection grid
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i < 100; i++) {
             SDL_SetRenderDrawColor(Grid::getGrid()->renderer, 0, 255, 0, 255);
             Grid::getGrid()->drawFunction(coef_id, 1.0f/i);
             SDL_SetRenderDrawColor(Grid::getGrid()->renderer, 255, 0, 0, 255);
